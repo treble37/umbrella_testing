f634bd3d47b8cd72210c46a5cd6c2342bde3a0f2
Bruce Park <brucepark0@gmail.com>
2019-05-15 14:07:27 -0700

    timber fork

--- apps/api/mix.exs
+++ apps/api/mix.exs
@@ -23,7 +23,7 @@ defmodule Api.MixProject do
   def application do
     [
       mod: {Api.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :mnesia]
     ]
   end
 
